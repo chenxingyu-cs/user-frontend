ede49d4115bdc3da6a7f228864377408ed4e6a7e
Xingyu Chen <chenxingyu.cs@gmail.com>
2017-04-23 19:38:52 -0700

    Add header

---
+++ src/components/MainLayout/Header.js
@@ -0,0 +1,15 @@
+import { NavBar, Icon } from 'antd-mobile';
+import React from 'react';
+
+function Header() {
+  return (
+    <NavBar leftContent="back" mode="light" onLeftClick={() => console.log('onLeftClick')}
+      rightContent={[
+        <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
+        <Icon key="1" type="ellipsis" />,
+      ]}
+    >NavBar</NavBar>
+  );
+}
+
+export default Header;
