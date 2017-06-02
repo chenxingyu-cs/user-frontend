dc08a5d96a3aa2f3563e98fe84d63ea6371f1ea9
Haoyun Wen <haoyun_wen@hotmail.com>
2017-06-01 22:36:16 -0700

    Revert "test"
    This reverts commit be0696eef0489fe9b4eb7144e671a911c79a233d.

--- src/utils/wechatOauth.js
+++ src/utils/wechatOauth.js
@@ -2,12 +2,13 @@ var OAuth = require('wechat-oauth');
 var client = new OAuth('wxf53ebc8656c7b6c2', 'ef3649c7af8eb1edcd3c6a2f56fd52b6');
 
 export function getUserinfo({code}) {
+  const res = {};
   wechat.getAccessToken(code, function (err, result) {
     var accessToken = result.data.access_token;
     var openid = result.data.openid;
     wechat.getUser(openid, function (err, result) {
       console.log(result);
-      const res = result;
+      res = result;
     });
   });
   return res;
